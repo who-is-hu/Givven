04836752cffb829fce2da6ec35ea756a8f6a81bd
YechanJin <roqhrryghl@gmail.com>
2020-06-01 22:32:00 +0900

    dapp client prototype
    labtop to desktop

--- dapp/src/test.js
+++ dapp/src/test.js
@@ -1,14 +1,29 @@
-import path from 'path';
 const fs = require('fs');
+var Web3 = require('web3');
 
-var Web3 = require("web3");
-const contractaddress = "0x3a210ba43667be2c579aba12decb6bba9f678346";
-web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+const contractaddress = '0x9e510d2Da0F4263f564835e606668bDC0f4Cc05B';
 
-fs.readFile('../build/Givven.json','utf8',(error,jsonFile)=>{
-    if(error) return console.log(error);
-    const jsonData = JSON.parse(jsonFile);
-    const abi = jsonData.abi;
-    console.log(abi);
-})
-var Givven = web3.eth.contract()
+//var web3 = new Web3(new Web3.providers.HttpProvider('http://127.0.0.1:8545/'));
+var web3 = new Web3();
+web3.setProvider('ws://127.0.0.1:8545');
+console.log(web3.eth.Contract);
+
+
+var jsonFile = fs.readFileSync('../build/contracts/Givven.json','utf8');
+var jsonData = JSON.parse(jsonFile);
+var abi = jsonData.abi;
+//console.log(abi);
+const givven = web3.eth.contract(abi).at(contractaddress);
+
+const getBalance = async () => {
+    try{
+        const r =  await givven.getBalance('jin');
+        return r;
+    } catch (err) 
+    {
+        console.err(err);
+    }
+
+}
+const t = getBalance();
+console.log(t);
