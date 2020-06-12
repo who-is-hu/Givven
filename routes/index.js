996d5cc6d8ec84676ab962fb80e3cdda275294a7
yachae85 <yachae85@gmail.com>
2020-06-12 22:18:58 +0900

    normal_donation_purchase_list 추가

--- routes/index.js
+++ routes/index.js
@@ -42,8 +42,8 @@ router.get('/charity_shop_main', function(req, res, next) {
 router.get('/normal_donation_detail_donate', function(req, res, next) {
   res.render('normal_donation_detail_donate.html');
 });
-router.get('/normal_donation_detail_purchase', function(req, res, next) {
-  res.render('normal_donation_detail_purchase.html');
+router.get('/normal_donation_detail_purchase_list', function(req, res, next) {
+  res.render('normal_donation_detail_purchase_list.html');
 });
 router.get('/normal_donation_detail', function(req, res, next) {
   res.render('normal_donation_detail.html');
@@ -68,9 +68,6 @@ router.get('/normal_shop_main', function(req, res, next) {
 });
 router.get('/normal_shop_search_result', function(req, res, next) {
   res.render('normal_shop_search_result.html');
-});
-router.get('/normal_donatiuon_detail_purchase_list', function(req, res, next) {
-  res.render('normal_donatiuon_detail_purchase_list.html.html');
 });
 /* seller */
 router.get('/seller_mypage_exchange', function(req, res, next) {
